771243e819ce97e2ea62d83ff8ddb931cb95fe8c
Lee Solway <lee@digitalanvil.co.uk>
2015-07-04 15:09:48 +0100

    Added basic plugin and template

---
+++ djangocms_owl/cms_plugins.py
@@ -0,0 +1,43 @@
+from django.utils.translation import ugettext as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .models import OwlCarousel
+
+
+class OwlCarouselPlugin(CMSPluginBase):
+    name = _('Owl Carousel')
+    model = OwlCarousel
+    allow_children = True
+    render_template = 'djangocms_owl/owl_carousel.html'
+    fieldsets = (
+        (None, {
+            'fields': (
+                'items',
+                'auto_height',
+            )
+        }),
+        (_('Navigation'), {
+            'fields': (
+                'pagination',
+                'pagination_numbers',
+                'navigation',
+
+            ),
+        }),
+        (_('AutoPlay'), {
+            'fields': (
+                'autoplay',
+                'stop_on_hover',
+            ),
+        }),
+        (_('Style'), {
+            'fields': (
+                'style',
+            ),
+        }),
+    )
+
+
+plugin_pool.register_plugin(OwlCarouselPlugin)
